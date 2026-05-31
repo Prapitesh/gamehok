c2445b31fa8e56836d1125275d24a144c892ca13
Prapitesh <aitipamulaprapitesh02@gmail.com>
2026-05-31 21:30:44 +0530

    backend fixes

--- src/main/java/org/ease/gamehok/config/SecurityConfig.java
+++ src/main/java/org/ease/gamehok/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://localhost:8887"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001","http://54.153.244.141:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
